5fc088d6f4b3bee23cffa2c5c645fb0470212ea8
yonax <crazy.yonax@gmail.com>
2017-07-21 18:51:53 +0500

    added bulma; simple counter

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,63 +1,63 @@
 module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
-
--- component import example
-import Components.Hello exposing ( hello )
+import Html.Events exposing (onClick)
+import Components.Hello exposing (hello)
 
 
--- APP
 main : Program Never Int Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.beginnerProgram { model = model, view = view, update = update }
+
 
 
 -- MODEL
-type alias Model = Int
+
+
+type alias Model =
+    Int
+
 
 model : number
-model = 0
+model =
+    0
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+
+
+type Msg
+    = NoOp
+    | Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        NoOp ->
+            model
+
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
 
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    div [ class "main container is-fluid" ]
+        [ div
+            [ class "block" ]
+            [ a [ class "button", onClick Decrement ] [ text "-" ]
+            , span [ class "tag is-primary is-medium" ] [ text <| toString model ]
+            , a [ class "button", onClick Increment ] [ text "+" ]
+            ]
         ]
-      ]
-    ]
-  ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
--- webpack.config.js
+++ webpack.config.js
@@ -24,106 +24,109 @@ console.log('WEBPACK GO! Building for ' + TARGET_ENV);
 
 // common webpack config (valid for dev and prod)
 var commonConfig = {
-    output: {
-        path: outputPath,
-        filename: `static/js/${outputFilename}`,
-    },
-    resolve: {
-        extensions: ['.js', '.elm'],
-        modules: ['node_modules']
-    },
-    module: {
-        noParse: /\.elm$/,
-        rules: [{
-            test: /\.(eot|ttf|woff|woff2|svg)$/,
-            use: 'file-loader?publicPath=../../&name=static/css/[hash].[ext]'
-        }]
-    },
-    plugins: [
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                postcss: [autoprefixer()]
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: 'src/static/index.html',
-            inject: 'body',
-            filename: 'index.html'
-        })
-    ]
+  output: {
+    path: outputPath,
+    filename: `static/js/${outputFilename}`,
+  },
+  resolve: {
+    extensions: ['.js', '.elm'],
+    modules: ['node_modules']
+  },
+  module: {
+    noParse: /\.elm$/,
+    rules: [{
+      test: /\.(eot|ttf|woff|woff2|svg)$/,
+      use: 'file-loader?publicPath=../../&name=static/css/[hash].[ext]'
+    }]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [autoprefixer()]
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/static/index.html',
+      inject: 'body',
+      filename: 'index.html'
+    })
+  ]
 }
 
 // additional webpack settings for local env (when invoked by 'npm start')
 if (isDev === true) {
-    module.exports = merge(commonConfig, {
-        entry: [
-            'webpack-dev-server/client?http://localhost:8080',
-            entryPath
-        ],
-        devServer: {
-            // serve index.html in place of 404 responses
-            historyApiFallback: true,
-            contentBase: './src',
-            hot: true
-        },
-        module: {
-            rules: [{
-                test: /\.elm$/,
-                exclude: [/elm-stuff/, /node_modules/],
-                use: [{
-                    loader: 'elm-webpack-loader',
-                    options: {
-                        verbose: true,
-                        warn: true,
-                        debug: true
-                    }
-                }]
-            },{
-                test: /\.sc?ss$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-            }]
-        }
-    });
+  module.exports = merge(commonConfig, {
+    entry: [
+      'webpack-dev-server/client?http://localhost:8080',
+      entryPath
+    ],
+    devServer: {
+      // serve index.html in place of 404 responses
+      historyApiFallback: true,
+      contentBase: './src',
+      hot: true
+    },
+    module: {
+      rules: [{
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [{
+          loader: 'elm-webpack-loader',
+          options: {
+            verbose: true,
+            warn: true,
+            debug: true
+          }
+        }]
+      }, {
+        test: /\.sc?ss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      }, {
+        test: /\.sass$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      }]
+    }
+  });
 }
 
 // additional webpack settings for prod env (when invoked via 'npm run build')
 if (isProd === true) {
-    module.exports = merge(commonConfig, {
-        entry: entryPath,
-        module: {
-            rules: [{
-                test: /\.elm$/,
-                exclude: [/elm-stuff/, /node_modules/],
-                use: 'elm-webpack-loader'
-            }, {
-                test: /\.sc?ss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'postcss-loader', 'sass-loader']
-                })
-            }]
-        },
-        plugins: [
-            new ExtractTextPlugin({
-                filename: 'static/css/[name]-[hash].css',
-                allChunks: true,
-            }),
-            new CopyWebpackPlugin([{
-                from: 'src/static/img/',
-                to: 'static/img/'
-            }, {
-                from: 'src/favicon.ico'
-            }]),
+  module.exports = merge(commonConfig, {
+    entry: entryPath,
+    module: {
+      rules: [{
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: 'elm-webpack-loader'
+      }, {
+        test: /\.sc?ss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      }]
+    },
+    plugins: [
+      new ExtractTextPlugin({
+        filename: 'static/css/[name]-[hash].css',
+        allChunks: true,
+      }),
+      new CopyWebpackPlugin([{
+        from: 'src/static/img/',
+        to: 'static/img/'
+      }, {
+        from: 'src/favicon.ico'
+      }]),
 
-            // extract CSS into a separate file
-            // minify & mangle JS/CSS
-            new webpack.optimize.UglifyJsPlugin({
-                minimize: true,
-                compressor: {
-                    warnings: false
-                }
-                // mangle:  true
-            })
-        ]
-    });
+      // extract CSS into a separate file
+      // minify & mangle JS/CSS
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        compressor: {
+          warnings: false
+        }
+        // mangle:  true
+      })
+    ]
+  });
 }
