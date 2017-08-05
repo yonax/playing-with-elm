332b77f4c3692f84ef8c5097e83c3d7192ae6b24
yonax <crazy.yonax@gmail.com>
2017-08-05 18:54:34 +0500

    added load task from fake api

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,27 +2,70 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode
 import Components.Task as Task
 
 
 main : Program Never Model Msg
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
+    Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
+
+
+
+-- Load data
+
+
+type alias Tasks =
+    List Task.Model
+
+
+getTasks : Http.Request Tasks
+getTasks =
+    Http.get "http://localhost:3000/tasks" decodeTasks
+
+
+decodeTasks : Decode.Decoder Tasks
+decodeTasks =
+    Decode.list decodeTask
+
+
+decodeTask : Decode.Decoder Task.Model
+decodeTask =
+    Decode.map2 Task.Model
+        (Decode.field "title" Decode.string)
+        (Decode.field "completed" Decode.bool)
+
+
+loadTasks : Cmd Msg
+loadTasks =
+    Http.send TasksLoaded getTasks
 
 
 
 -- MODEL
 
 
+init : ( Model, Cmd Msg )
+init =
+    ( model, loadTasks )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
 type alias Model =
     { entries : List Task.Model
+    , error : Maybe Http.Error
     }
 
 
 model : Model
 model =
-    { entries = [ Task.init "Hello", Task.init "Hello 2" ]
+    { entries = []
+    , error = Nothing
     }
 
 
@@ -32,14 +75,21 @@ model =
 
 type Msg
     = NoOp
+    | TasksLoaded (Result Http.Error Tasks)
     | TaskMsg Int Task.Msg
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            model
+            ( model, Cmd.none )
+
+        TasksLoaded (Ok tasks) ->
+            ( { model | entries = tasks, error = Nothing }, Cmd.none )
+
+        TasksLoaded (Err error) ->
+            ( { model | error = Just error }, Cmd.none )
 
         TaskMsg index msg ->
             let
@@ -49,7 +99,7 @@ update msg model =
                     else
                         taskModel
             in
-                { model | entries = List.indexedMap (update msg) model.entries }
+                ( { model | entries = List.indexedMap (update msg) model.entries }, Cmd.none )
 
 
 
@@ -60,4 +110,6 @@ view : Model -> Html Msg
 view model =
     section
         [ class "section" ]
-        (List.indexedMap (\index t -> Task.view t |> Html.map (TaskMsg index)) model.entries)
+        ([ div [] [ text <| toString model.error ] ]
+            ++ (List.indexedMap (\index t -> Task.view t |> Html.map (TaskMsg index)) model.entries)
+        )
